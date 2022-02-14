b4208886893bd5ce39a33eef21bac344418e322a
Claudius Holeksa <mail@keldu.de>
2022-02-14 22:22:55 +0100

    2 space instead of 1 tab

--- pkgs/tools/audio/midimonster/default.nix
+++ pkgs/tools/audio/midimonster/default.nix
@@ -16,37 +16,37 @@
 }:
 
 stdenv.mkDerivation rec {
-	pname = "midimonster";
-	version = "0.6.0";
+  pname = "midimonster";
+  version = "0.6.0";
 
-	buildInputs = [pkg-config gnumake gcc lua5_4 openssl jack1 python3 alsa-lib ncurses libevdev];
+  buildInputs = [pkg-config gnumake gcc lua5_4 openssl jack1 python3 alsa-lib ncurses libevdev];
 	
-	src = fetchFromGitHub {
-		repo = "midimonster";
-		owner = "cbdevnet";
-		rev = "f16f7db86662fcdbf45b6373257c90c824b0b4b0";
-		sha256 = "131zs4j9asq9xl72cbyi463xpkj064ca1s7i77q5jrwqysgy52sp";
-	};
+  src = fetchFromGitHub {
+    repo = "midimonster";
+    owner = "cbdevnet";
+    rev = "f16f7db86662fcdbf45b6373257c90c824b0b4b0";
+    sha256 = "131zs4j9asq9xl72cbyi463xpkj064ca1s7i77q5jrwqysgy52sp";
+};
 
-	doCheck = true;
-	enableParallelBuilding = true;
+  doCheck = true;
+  enableParallelBuilding = true;
 
-	outputs = ["out" "man"];
+  outputs = ["out" "man"];
 
-	installPhase = ''
-		PREFIX=$out make install
+  installPhase = ''
+    PREFIX=$out make install
 
-		mkdir -p "$man/share/man/man1"
-		cp assets/midimonster.1 "$man/share/man/man1"
+    mkdir -p "$man/share/man/man1"
+    cp assets/midimonster.1 "$man/share/man/man1"
 
-		mkdir -p "$out/share/icons/hicolor/scalable/apps"
-		cp assets/MIDIMonster.svg "$out/share/icons/hicolor/scalable/apps/"
-	'';
+    mkdir -p "$out/share/icons/hicolor/scalable/apps"
+    cp assets/MIDIMonster.svg "$out/share/icons/hicolor/scalable/apps/"
+  '';
 
-	meta = with lib; {
-		homepage = "https://midimonster.net";
-		description = "Multi-protocol translation tool";
-		platforms = platforms.unix;
-		maintainers = with maintainers; [keldu];
-	};
+  meta = with lib; {
+    homepage = "https://midimonster.net";
+    description = "Multi-protocol translation tool";
+    platforms = platforms.unix;
+    maintainers = with maintainers; [keldu];
+  };
 }
